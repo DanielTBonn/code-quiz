bddc364c807593963dac825ce55ebdd704d618ba
Daniel Bonn <danieltbonn@gmail.com>
2023-06-30 14:45:00 -0500

    added some styling changes to the css .btn class and created an object to determine how question/answers will be stored

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,21 @@ var btnClick = document.querySelector(".btn");
 var timeLeft = 75;
 
 // Decrements timer (will use for wrong answers)
+// Need to fix error where only the first .btn class is chosen
 btnClick.addEventListener("click", () => {
     timeLeft = timeLeft - 5;
 });
+
+const quizSheet = {
+  question: "This is the first question",
+  answers: {
+    answer1: {answer: "This is the first answer", value: true},
+    answer2: {answer: "This is the second answer", value: false},
+    answer3: {answer: "This is the third answer", value: false},
+    answer4: {answer: "This is the fourth answer", value: false},
+  }
+}
+console.log(quizSheet.answers.answer1);
 
 function countdown() {
     var timeInterval = setInterval(function () {
