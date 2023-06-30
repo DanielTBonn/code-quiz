97dd0f5213b1481c0a5603f2220a557f9f3d872a
Daniel Bonn <danieltbonn@gmail.com>
2023-06-29 21:18:17 -0500

    added a function to decrement time

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,20 @@
 var timerEl = document.querySelector(".countdown");
+var btnClick = document.querySelector(".btn");
+var timeLeft = 20;
+
+// Decrements timer (will use for wrong answers)
+btnClick.addEventListener("click", () => {
+    timeLeft = timeLeft - 5;
+});
 
 function countdown() {
-    var timeLeft = 5;
 
     var timeInterval = setInterval(function () {
 
       timerEl.textContent = timeLeft + ' seconds remanining';
       timeLeft--;
       if (timeLeft === 0) {
+        // End Quiz Here
         clearInterval(timeInterval);
       }
     }, 1000);
