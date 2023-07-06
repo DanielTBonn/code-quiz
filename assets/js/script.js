6496773ac6494c08b5a43e00b47f8a151fb96e7f
Daniel Bonn <danieltbonn@gmail.com>
2023-07-05 21:28:50 -0500

    adding functionality for changing questions on click as well as verifying correctness, fixed bug where only the first button was queried

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 var timerEl = document.querySelector(".countdown");
-var btnClick = document.querySelector(".btn");
+var btnClick = document.querySelector(".card");
 var timeLeft = 75;
 
-// Decrements timer (will use for wrong answers)
-// Need to fix error where only the first .btn class is chosen
-btnClick.addEventListener("click", () => {
-    timeLeft = timeLeft - 5;
-});
+var i = 0;
+var quiz = [];
 
 const quizSheet = {
   question1: {
@@ -16,7 +13,7 @@ const quizSheet = {
       {answer: "This is the second answer", value: false},
       {answer: "This is the third answer", value: false},
       {answer: "This is the fourth answer", value: false}
-      ]
+    ]
   },
   question2: {
     questionText: "This is the second question",
@@ -25,71 +22,100 @@ const quizSheet = {
       {answer: "This is true answer", value: true},
       {answer: "This answer is untrue", value: false},
       {answer: "This answer is untrue", value: false}
-      ]
+    ]
   }
+}
+
+for (item in quizSheet) {
+  quiz.push(quizSheet[item]);
+} 
+
+// Decrements timer (will use for wrong answers)
+// Need to fix error where only the first .btn class is chosen
+btnClick.addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON";
+  console.log(quiz[i].answers[0].value);
+  if (isButton) {
+    console.log("Truuuuuue");
+    
+    if (null) {
+      timeLeft = timeLeft - 5;
+    }
+
+    if (timeLeft > 0 && i < quiz.length - 1){
+      i++;
+      displayQuestion(quiz[i]);
+    }
+  }
+});
+
+for (item in quizSheet) {
+  console.log(item);
+  console.log(typeof item);
+  console.log(quizSheet[item].questionText);
 }
 
 function displayQuestion(question) {
   var changeQuestion = document.querySelector(".question-content");
-  changeQuestion.textContent = quizSheet.question1.questionText;
+  changeQuestion.textContent = question.questionText;
   
   for (let i = 1; i < 5; i++) {
     var btnnum = ".btn"
     btnnum += i;
     var changeText = document.querySelector(btnnum);
-    changeText.textContent = quizSheet.question1.answers[i - 1].answer;
+    changeText.textContent = question.answers[i - 1].answer;
+    changeText.setAttribute("data-number") === (i - 1);
   }
 }
 
 function questionLoop() {
+  
   for (const question in quizSheet) {
     console.log(question);
     console.log(typeof question);
     console.log(quizSheet[question].questionText);
-    clickQuestion();
   }
 }
 
-async function clickQuestion() {
-  var click = await btnClick.addEventListener("click", null);
-}
-
-questionLoop();
-
-displayQuestion("hi");
-
-if (!localStorage.getItem("bgcolor")) {
-  populateStorage();
-} else {
-  setStyles();
-}
-
-function populateStorage() {
-  localStorage.setItem("btn1", document.querySelector(".btn1").innerHTML);
-
-  setStyles();
-}
-
-console.log(localStorage);
-
-function setStyles() {
-  const btnInnerHtml = localStorage.getItem("btn1");
-}
-
-
 
 function countdown() {
-  var timeInterval = setInterval(function () {
 
+  var timeInterval = setInterval(function () {
+    
     timerEl.textContent = timeLeft + ' seconds remanining';
     timeLeft--;
-    
+
     if (timeLeft <= 0) {
       // End Quiz Here
       timerEl.textContent = 0 + ' seconds remanining';
       clearInterval(timeInterval);
     }
+
   }, 1000);
 }
 
 countdown();
+displayQuestion(quiz[0]);
+
+
+
+// example that will later turn into the scores for the user
+// if (!localStorage.getItem("bgcolor")) {
+//   populateStorage();
+// } else {
+//   setStyles();
+// }
+
+// function populateStorage() {
+//   localStorage.setItem("btn1", document.querySelector(".btn1").innerHTML);
+
+//   setStyles();
+// }
+
+// console.log(localStorage);
+
+// function setStyles() {
+//   const btnInnerHtml = localStorage.getItem("btn1");
+// }
+
+
