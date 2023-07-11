b7bd9c088194ed446772275f26b175242b149462
Daniel Bonn <danieltbonn@gmail.com>
2023-07-10 20:51:04 -0500

    added storage for items and a list of scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,23 +4,43 @@ var ulEl = document.querySelector("#history");
 
 
 console.log(initials, scores);
+console.log(localStorage.getItem("items"));
 
-var items = [];
+function getStorage() { 
+    
+    var items = [];
+    if (localStorage.items) {
+        console.log("its there!");
+        items = JSON.parse(localStorage.items);
+    } else {
+        console.log(false);
+    }
 
-if (localStorage.items) {
-    console.log("its there!");
-    items = JSON.parse(localStorage.items);
-} else {
-    console.log(false);
+    return items;
 }
 
-items.push([initials, scores]);
+
+function addStorage(items, intials, scores) {
+    items.push([initials, scores]);
+    localStorage.setItem("items", JSON.stringify(items));
+    
+}
+
+items = getStorage();
+addStorage(items, initials, scores);
+
 console.log(items);
 
-localStorage.setItem("item", JSON.stringify(items));
-console.log(localStorage.getItem("item"));
+console.log(localStorage);
+console.log(localStorage.getItem("items"));
 
-var listEl = document.createElement("li");
-listEl.textContent = items[0][0] + " - " + items[0][1];
-ulEl.append(listEl);
+function createScores(items) {
+    for (let i = 0; i < items.length; i++) {
+        var listEl = document.createElement("li");
+        listEl.textContent = items[i][0] + " - " + items[i][1];
+        ulEl.append(listEl);
+    }
+}
+
+createScores(items);
 
