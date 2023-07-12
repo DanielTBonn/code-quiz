02341c73ea8f75a087163afd9e1b177794441727
Daniel Bonn <danieltbonn@gmail.com>
2023-07-11 22:18:06 -0500

    trying to add sorting functions to scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -11,12 +11,12 @@ function getStorage() {
     return items;
 }
 
-function addStorage(items, intials, scores) {
+function addStorage(items, initials, scores) {
     if (initials === 'null' || scores === 'null') {
         return;
     }
 
-    items.push([initials, scores]);
+    items = bblSort(items);
     localStorage.setItem("items", JSON.stringify(items));
     localStorage.setItem("initials", null);
     localStorage.setItem("score", null);
@@ -25,10 +25,36 @@ function addStorage(items, intials, scores) {
 function createScores(items) {
     for (let i = 0; i < items.length; i++) {
         var listEl = document.createElement("li");
-        listEl.textContent = items[i][0] + " - " + items[i][1];
+        listEl.textContent = items[i][1] + " - " + items[i][0];
         listEl.setAttribute("class", "list-group-item");
         ulEl.append(listEl);
     }
+}
+
+// Creating the bblSort function
+function bblSort(arr) {
+  
+    for (var i = 0; i < arr.length; i++) {
+  
+        // Last i elements are already in place  
+        for (var j = 0; j < (arr.length - i - 1); j++) {
+  
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (Number(arr[j][0]) > Number(arr[j + 1][0])) {
+  
+                // If the condition is true
+                // then swap them
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+  
+    // Print the sorted array
+    console.log(arr);
+    return arr;
 }
 
 items = getStorage();
@@ -41,7 +67,7 @@ clearBtn.addEventListener('click', () => {
     window.location.href = "./index.html";
 })
 
-
-
-
+console.log(typeof items[0][1]);
+bblSort(items);
+console.log("hmm")
 
